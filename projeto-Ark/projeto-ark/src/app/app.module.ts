0a7d8852591f44a8b738152088601d613bf7f876
mateussuzuki <mateussuzuki2022@hotmail.com>
2023-08-21 19:14:51 -0300

    navbar

--- projeto-Ark/projeto-ark/src/app/app.module.ts
+++ projeto-Ark/projeto-ark/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,

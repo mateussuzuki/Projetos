11561e5e5e002fe6f56b0b7c793b2992a203acdb
mateussuzuki <mateussuzuki2022@hotmail.com>
2023-08-25 19:15:47 -0300

    formulário

--- projeto-Ark/projeto-ark/src/app/components/formulario/formulario.component.ts
+++ projeto-Ark/projeto-ark/src/app/components/formulario/formulario.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.scss']
 })
-export class FormularioComponent {
-  
+export class FormularioComponent implements OnInit{
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
 }

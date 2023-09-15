9323e8f9e0ce2d3cd0f97059ef3c9681e8017227
mateussuzuki <mateussuzuki2022@hotmail.com>
2023-09-14 21:55:44 -0300

    arrumando os destaques

--- projeto-Ark/projeto-ark/src/app/components/bemvindo/bemvindo.component.ts
+++ projeto-Ark/projeto-ark/src/app/components/bemvindo/bemvindo.component.ts
@@ -10,7 +10,7 @@ export class BemvindoComponent {
 
     conteudo = [
     new Conteudo('/assets/imagens-destaque/ark2.png', 'Akr2', 'Venha conferir as novas atualizações do Ark 2'),
-    new Conteudo('/assets/imagens-destaque/ark-genesis.jpeg', 'Ark genesis', 'Conheça o novo mapa DLC com muitas novidades!'),
+    new Conteudo('/assets/imagens-destaque/ark-genesis.jpeg', 'Ark genesis', 'Conheça o novo mapa Ark Genesis com muitas novidades e novas criaturas!'),
     new Conteudo('/assets/imagens-destaque/amargassaurus.webp', 'Argamassaurus', 'Criatura nova vem junto com o novo mapa The Lost Island'),
   ]
   
